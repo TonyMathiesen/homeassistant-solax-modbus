c34545d314ac3c6ccbaead5b4de04d2df6373889
TonyMathiesen <95135900+TonyMathiesen@users.noreply.github.com>
2024-09-26 13:33:46 +0200

    Update plugin_growatt.py

--- custom_components/solax_modbus/plugin_growatt.py
+++ custom_components/solax_modbus/plugin_growatt.py
@@ -4470,15 +4470,16 @@ SENSOR_TYPES: list[GrowattModbusSensorEntityDescription] = [
         allowedtypes = GEN3 | HYBRID,
         entity_registry_enabled_default = False,
         entity_category = EntityCategory.DIAGNOSTIC,
+        #internal = True,
     ),
     GrowattModbusSensorEntityDescription(
         name = "Time 1 End",
-        key = "time_1_end",
+        key = "time_1_end_read",
         register = 3039, #TL-XH GEN3 load/battery/grid first priority
         allowedtypes = GEN3 | HYBRID,
-        option_dict = TIME_OPTIONS_GEN4,
         entity_registry_enabled_default = False,
         entity_category = EntityCategory.DIAGNOSTIC,
+        #internal = True,
     ),  
     #####
     #
