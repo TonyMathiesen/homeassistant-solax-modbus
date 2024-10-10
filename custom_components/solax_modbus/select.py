d2300aff5af084b78ac8455c2e7f796a0835e81c
TonyMathiesen <95135900+TonyMathiesen@users.noreply.github.com>
2024-10-10 15:05:01 +0200

    Update select.py

--- custom_components/solax_modbus/select.py
+++ custom_components/solax_modbus/select.py
@@ -1,5 +1,5 @@
 from .const import ATTR_MANUFACTURER, DOMAIN, CONF_MODBUS_ADDR, DEFAULT_MODBUS_ADDR
-from .const import WRITE_DATA_LOCAL, WRITE_MULTISINGLE_MODBUS, WRITE_SINGLE_MODBUS
+from .const import WRITE_DATA_LOCAL, WRITE_MULTISINGLE_MODBUS, WRITE_SINGLE_MODBUS, TMPDATA_EXPIRY
 from homeassistant.components.select import PLATFORM_SCHEMA, SelectEntity
 from homeassistant.const import CONF_NAME
 from homeassistant.core import callback
